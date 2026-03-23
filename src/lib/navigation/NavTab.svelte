e2bb92e8a1ef7992476f771464257d92e5fd7ecc
Mishi <mishi.beraia@gmail.com>
2026-03-23 10:20:18 +0400

    Added persistent navtab highlighting for child pages as well

--- src/lib/navigation/NavTab.svelte
+++ src/lib/navigation/NavTab.svelte
@@ -5,7 +5,7 @@
 
 
 <a 
-    class="btn flex-1 border-b-2 transition-all {page.url.pathname === '/' + tab_name ? 'text-orange-800 border-emerald-800 dark:text-orange-500 dark:border-orange-500 font-bold' : 'border-transparent'}" 
+    class="btn flex-1 border-b-2 transition-all {page.url.pathname.startsWith('/' + tab_name) ? 'text-orange-800 border-emerald-800 dark:text-orange-500 dark:border-orange-500 font-bold' : 'border-transparent'}" 
     href="/{tab_name}"
 >
     {tab_name}
